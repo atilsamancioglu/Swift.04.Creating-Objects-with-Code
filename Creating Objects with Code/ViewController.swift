2e8daa4d6edf644a5485c6e27e513ff7efbbb473
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-02-12 20:08:29 +0300

    final version

--- Creating Objects with Code/ViewController.swift
+++ Creating Objects with Code/ViewController.swift
@@ -10,15 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let myButton = UIButton()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    
+    let width = view.frame.size.width
+    let height = view.frame.size.height
+        
+    let myLabel = UILabel()
+    myLabel.text = "My coding label!"
+    myLabel.textAlignment = .center
+    myLabel.frame = CGRect(x: width * 0.1, y: height * 0.3, width: width * 0.8 , height: 50)
+    view.addSubview(myLabel)
+    
+        
+        myButton.frame = CGRect(x: width * 0.2, y: height * 0.5, width: width * 0.6, height: 50)
+        myButton.setTitle("myCoding Button", for: UIControlState.normal)
+        myButton.setTitleColor(UIColor.darkGray, for: UIControlState.normal)
+        view.addSubview(myButton)
+        
+        myButton.addTarget(self, action: #selector(ViewController.myAction), for: UIControlEvents.touchUpInside)
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    func myAction() {
+        print("You have clicked my button")
+        self.myButton.setTitleColor(UIColor.blue, for: UIControlState.normal)
+    }
+    
+    
+
 
 
 }
